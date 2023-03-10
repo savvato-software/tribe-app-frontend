1cd0f7feeb30b0519957d47b892b4381ef883de7
MARIEDIAOUNE <marie.diaoune@gmail.com>
2023-03-10 08:57:47 -0700

    Working on creating new Attributes and saving it to User

--- src/app/pages/attributes-page/_services/attributes.model.service.ts
+++ src/app/pages/attributes-page/_services/attributes.model.service.ts
@@ -18,4 +18,33 @@ export class AttributesModelService {
                 private _constants: Constants) {
 
     }
+
+    save(phrase) {
+        const self = this;
+        return new Promise((resolve, reject) => {
+            this._attributesApiService.save(phrase).then((rtn) => {
+                    console.log("Call to attributeApiService was successfull");
+
+                    resolve({ "successful": rtn });
+                (err) => {
+                    reject(err);
+                }
+            });
+                // if (model["isImageChanged"]) {
+                    
+
+                //         self._pictureService.save(this._constants.PHOTO_TYPE_PROFILE, model).then((data) => {
+                //             console.log("changed image for " + model['id'] + " successfully saved on server.")
+                //             model["isImageChanged"] = false;
+                //             resolve(rtn);
+                //         }, (err) => {
+                //             console.log("Error with saving picture in call to pictureService!")
+                //             reject(err);
+                //         })
+                // } else {
+                //     resolve(rtn);
+                // }
+            //})
+        });
+    }
 }
