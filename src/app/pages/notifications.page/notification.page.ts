44124cad5983e05e8de1a59047a77449f89edfb1
loretdemolas <sebast.loret@gmail.com>
2023-04-11 13:15:07 -0400

    TRIB-83
    Added:
    notification.page.scss
    -blank page, yet to run the app
    notification.page.html
    -Added Header 'Notifications'
    notification.page.ts
    -created basic component for notification page
    notification.page.module.ts
    -create basic Module

---
+++ src/app/pages/notifications.page/notification.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {AuthService} from "@savvato-software/savvato-javascript-services";
+
+@Component({
+selector: 'page-notification',
+templateUrl: './notification.page.html',
+styleUrls: ['./notification.page.scss']
+})
+
+export class NotificationPage implements OnInit {}
+
