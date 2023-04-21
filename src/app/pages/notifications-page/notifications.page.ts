73e14994730195995edb70e8c8b31be3bba5f6b8
loretdemolas <sebast.loret@gmail.com>
2023-04-20 21:32:14 -0400

    TRIB-84: Implement notifications api pt2
    long-press.directive.ts:
    Created a directive that use mousedown, mouseleave, mouseup, and timout to implement a longpress click.
    
    notifications.page.module.ts:
    importated and declared directive
    
    notifications.page.html:
    Added longpress event listenter to call onDelete function.
    Added click event listerner to consol log more information on single click
    Added CSS class to give user feedback when longpressing.
    
    notifications.page.scss:
    Added CSS to change ionitem color. Currently doesn't work. For some reason, animations break the longpress directive.
    
    notifications.page.ts:
    Added showMore function to console.log more mock information on each notification.

--- src/app/pages/notifications-page/notifications.page.ts
+++ src/app/pages/notifications-page/notifications.page.ts
@@ -6,28 +6,32 @@ selector: 'page-notification',
 templateUrl: './notifications.page.html',
 styleUrls: ['./notifications.page.scss']
 })
+export class NotificationPage implements OnInit {
+notifications: any[];
 
-export class NotificationPage implements OnInit{
-    notifications: any[];
+constructor(private notificationService: NotificationService) {}
 
-    constructor(private notificationService: NotificationService) {}
-  
-    ngOnInit() {
-      // Call the getMessages() method of the notification service to load all notifications
-      this.notificationService.getMessages().subscribe(notifications => {
-        this.notifications = notifications;
-      });
-    }
-    getIcon(notifications: any): string {
-        const icon = notifications.icon
-        return icon ? icon : '';
-    }
-  
-    onDeleteNotification(id: number) {
-      // Call the deleteMessage() method of the notification service to delete a notification
-      this.notificationService.deleteMessage(id).subscribe(() => {
-        // Remove the deleted notification from the local array of notifications
-        this.notifications = this.notifications.filter(notification => notification.id !== id);
-      });
-    }
+  ngOnInit() {
+    // Call the getMessages() method of the notification service to load all notifications
+    this.notificationService.getMessages().subscribe(notifications => {
+      this.notifications = notifications;
+    });
   }
+
+  getIcon(notification: any): string {
+    const icon = notification.icon;
+    return icon ? icon : '';
+  }
+
+  onDeleteNotification(id: number) {
+    // Call the deleteMessage() method of the notification service to delete a notification
+    this.notificationService.deleteMessage(id).subscribe(() => {
+      // Remove the deleted notification from the local array of notifications
+      this.notifications = this.notifications.filter(notification => notification.id !== id);
+    });
+  }
+
+  onShowMoreInfo(notification: any) {
+    console.log('More information: ', notification);
+  }
+}
