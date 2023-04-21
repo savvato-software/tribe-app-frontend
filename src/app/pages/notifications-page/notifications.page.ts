8c90130063a8fb8b5930bd2cd47a3069d3625a42
loretdemolas <sebast.loret@gmail.com>
2023-04-20 20:08:58 -0400

    TRIB-84: Implement notifications api:
    notifications.api.service.ts:
    
    Created a mock api that simulates data. It takes two arrays, one for item and the other for message. It builds the notifications with the correct icon:message pairing.
    
    notifications.page.html:
    
    Added an ion List that uses a for loop to create list items. Each item has an onlick event listern to remove the notification from the notification array and display. If no notifications are displayed, it will display "no new notificaion"
    
    notifications.page.ts:
    
    Added logic to handleDelete of the notificaion.
    
    Added a function to get notification icon and display it first in each list.
    
    Added oninit lifecycle to call the api service and get notitications.

--- src/app/pages/notifications-page/notifications.page.ts
+++ src/app/pages/notifications-page/notifications.page.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-
-import {AuthService} from "@savvato-software/savvato-javascript-services";
+import { Component, OnInit } from '@angular/core';
+import { NotificationService } from './_service/notifications.api.service';
 
 @Component({
 selector: 'page-notification',
@@ -8,5 +7,27 @@ templateUrl: './notifications.page.html',
 styleUrls: ['./notifications.page.scss']
 })
 
-export class NotificationPage {}
+export class NotificationPage implements OnInit{
+    notifications: any[];
 
+    constructor(private notificationService: NotificationService) {}
+  
+    ngOnInit() {
+      // Call the getMessages() method of the notification service to load all notifications
+      this.notificationService.getMessages().subscribe(notifications => {
+        this.notifications = notifications;
+      });
+    }
+    getIcon(notifications: any): string {
+        const icon = notifications.icon
+        return icon ? icon : '';
+    }
+  
+    onDeleteNotification(id: number) {
+      // Call the deleteMessage() method of the notification service to delete a notification
+      this.notificationService.deleteMessage(id).subscribe(() => {
+        // Remove the deleted notification from the local array of notifications
+        this.notifications = this.notifications.filter(notification => notification.id !== id);
+      });
+    }
+  }
