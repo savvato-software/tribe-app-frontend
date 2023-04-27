d8f391f5f3dfa8c45fc0854954cf923695ea8066
Ashton Hanke <ashguy121@gmail.com>
2023-04-26 19:25:25 -0600

    added sign in test and forgot password button test

--- cypress/e2e/login.page.cy.ts
+++ cypress/e2e/login.page.cy.ts
@@ -3,8 +3,20 @@ describe('empty spec', () => {
         cy.visit('http://localhost:8100/login')
     })
 
+    it('checks reactivity of sing in email address and password and clicks the sign in button', () => {
+        cy.get('[data-test="emailaddress-input"]').type("testuser@tribeapp.com")
+        cy.get('[data-test="password-input"]').type("admin")
+        cy.get('[data-test="sign-in-btn"]').click()
+
+  })
+
     it('visits the login page and clicks the create new user button', () => {
      cy.get('[data-test="create-new-user-btn"]').click()
+
+  })
+
+    it('visits the login page and clicks the forgot password button', () => {
+     cy.get('[data-test="forgot-password-btn"]').click()
 
   })
 
