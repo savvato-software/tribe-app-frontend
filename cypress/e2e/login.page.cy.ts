06a5cf6b53f70abed5071eb917d314c6998e7fa2
Ashton Hanke <ashguy121@gmail.com>
2023-04-26 18:47:41 -0600

    fixed login page test

--- cypress/e2e/login.page.cy.ts
+++ cypress/e2e/login.page.cy.ts
@@ -1,10 +1,12 @@
 describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:8100/login')
-    cy.get('[data-test="create-new-user-btn"]').click()
+    beforeEach(() => {
+        cy.visit('http://localhost:8100/login')
+    })
 
-    cy.get(".new-user-page-content ion-input").should('have.length', 5)
+    it('visits the login page and clicks the create new user button', () => {
+     cy.get('[data-test="create-new-user-btn"]').click()
+
   })
 
-  it('')
+
 })
