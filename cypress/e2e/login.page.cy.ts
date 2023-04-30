4d869574be2a1d61a2993316ec0bf798ef20785f
Ashton Hanke <ashguy121@gmail.com>
2023-04-29 19:44:27 -0600

    fixed spelling errors

--- cypress/e2e/login.page.cy.ts
+++ cypress/e2e/login.page.cy.ts
@@ -3,7 +3,7 @@ describe('empty spec', () => {
         cy.visit('http://localhost:8100/login')
     })
 
-    it('checks reactivity of sing in email address and password and clicks the sign in button', () => {
+    it('checks reactivity of sign in email address and password and clicks the sign in button', () => {
         cy.get('[data-test="emailaddress-input"]').type("testuser@tribeapp.com")
         cy.get('[data-test="password-input"]').type("admin")
         cy.get('[data-test="sign-in-btn"]').click()
