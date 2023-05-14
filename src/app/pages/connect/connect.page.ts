94748268a2f2c8222f6b25b37db67126bea8036d
Sung Kim <code.mechanica@gmail.com>
2023-05-14 12:11:30 +0900

    first attempt not working

---
+++ src/app/pages/connect/connect.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connect',
+  templateUrl: './connect.page.html',
+  styleUrls: ['./connect.page.scss'],
+})
+export class ConnectPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
