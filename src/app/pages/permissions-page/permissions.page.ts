1a2cb90f9571aae4cc5afe107783280e7d745859
Jason Jacobs <56858451+Jayj30pro@users.noreply.github.com>
2023-06-01 20:26:06 -0600

    TRIB-114 Frontend - Permissions: Display the user name of each current user (#31)
    * TRB-30-dropbox-buttons -ng test-routing Error2
    
    * error fix
    
    * added permissionsApi+permissions
    model W/UserList
    
    * Ticket TRIB-114 completion
    
    * one model one init
    
    * TRIB-115 + Transforming Services

--- src/app/pages/permissions-page/permissions.page.ts
+++ src/app/pages/permissions-page/permissions.page.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@savvato-software/savvato-javascript-services';
+import { PermissionsModelService } from './_services/permissions.model.service';
+import {LoadingService} from "../../_services/loading-spinner/loading.service";
+
+
 
 
 
@@ -12,14 +18,61 @@ import { Component } from '@angular/core';
 
 export class PermissionsPage {
 
-  permissions = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "reviewer",
-    "observer"
-  ]
+
+  constructor( private router: Router,
+    private _authService: AuthService,
+    private _permissionsModelService: PermissionsModelService,
+    private _loadingService: LoadingService) {
+
+  }
+
+  
+
+  ionViewWillEnter() {
+    this._loadingService.show({message: "..loading.."}).then(() => { 
+      this._permissionsModelService.init();
+      this._loadingService.dismiss ();
+    })
+  }
+
+  hasUser = false;
+
+  selectedSkills = [];
+
+  theCurrentUser = this.getUser();
+
+  selectedUser = {};
+
+  
+  
+  selectUser(user) {
+    let role = [];
+    for (let i of user.roles){
+    role.push(i.name);
+    }
+    this.selectedUser = user;
+    this.selectedSkills = role;
+    this.hasUser = true;
+  }
+  
+  clearUser() {
+    this.selectedSkills = [];
+    this.hasUser = false;
+    
+  }
+
+  getUser() {
+    return this._authService.getUser();
+  }
+  
+  
+  getListOfUsers() {
+    return this._permissionsModelService.getListOfUsers();
+  }
+
+  getListOfRoles() {
+    return this._permissionsModelService.getListOfRoles();
+  }
 
   saveChanges() {
 
@@ -29,6 +82,13 @@ export class PermissionsPage {
 
   }
 
+  
+
+
+  navigateTo(url?: string) {
+    url = url || 'nav';
+    this.router.navigate([url], { replaceUrl: true });
+  }
 }
 
 
