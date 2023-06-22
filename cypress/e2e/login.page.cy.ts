164e970c35f60325f3c9471736a1a8ef1a7409e4
Johnathan James <haxwell@gmail.com>
2023-06-22 12:54:25 -0600

    Corrected a login page cypress test after recent changes.

--- cypress/e2e/login.page.cy.ts
+++ cypress/e2e/login.page.cy.ts
@@ -4,8 +4,12 @@ describe('empty spec', () => {
     })
 
     it('checks reactivity of sign in email address and password and clicks the sign in button', () => {
-        cy.get('[data-test="emailaddress-input"]').type("testuser@tribeapp.com")
-        cy.get('[data-test="password-input"]').type("admin")
+        cy.get('[data-test="emailaddress-input"] input').clear();
+        cy.get('[data-test="emailaddress-input"] input').focus();
+        cy.get('[data-test="emailaddress-input"] input').type("testuser@tribeapp.com")
+        cy.get('[data-test="password-input"] input').clear();
+        cy.get('[data-test="password-input"] input').focus();
+        cy.get('[data-test="password-input"] input').type("admin")
         cy.get('[data-test="sign-in-btn"]').click()
         cy.url().should('eq', 'http://localhost:8100/home')
   })
