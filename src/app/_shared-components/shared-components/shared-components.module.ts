23137965333ac5d89e496a629c08744797be5d82
Audra Wathen <mrsbluerose@gmail.com>
2023-06-23 11:56:53 -0600

    TRIB-132: imports ionic module to shared components module

--- src/app/_shared-components/shared-components/shared-components.module.ts
+++ src/app/_shared-components/shared-components/shared-components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
+import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   exports: [HeaderComponent]
 })
