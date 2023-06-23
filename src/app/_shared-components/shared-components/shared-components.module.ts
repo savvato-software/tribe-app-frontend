d83dcf4813e8d33276900c20f9d57755270d3fa5
Audra Wathen <mrsbluerose@gmail.com>
2023-06-23 11:51:31 -0600

    TRIB-132: adds header componenet to shared-components declarations and exports arrays

--- src/app/_shared-components/shared-components/shared-components.module.ts
+++ src/app/_shared-components/shared-components/shared-components.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { HeaderComponent } from '../header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [HeaderComponent]
 })
 export class SharedComponentsModule { }
