b5ea8fab8218c521382b43220ae8f43c28dea207
Sung Kim <code.mechanica@gmail.com>
2023-06-28 11:24:46 +0900

    Having trouble with the routing code. I'll talk to you Thursday about it

--- src/app/pages/connect/connect-routing.module.ts
+++ src/app/pages/connect/connect-routing.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ConnectPage } from './connect.page';
+import { ListConnectionsPage } from './list-connections/list-connections.page';
+import { OpenConnectionPage } from './open-connection/open-connection.page';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'Connect',
     component: ConnectPage
   },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./open-connection/open-connection.module').then( m => m.OpenConnectionPageModule)
+  // },
   {
-    path: 'open-connection',
-    loadChildren: () => import('./open-connection/open-connection.module').then( m => m.OpenConnectionPageModule)
+    path: 'List-Connections',
+    component: ListConnectionsPage
+  },
+  {
+    path: 'Open-connection',
+    component: OpenConnectionPage
   }
 ];
 
