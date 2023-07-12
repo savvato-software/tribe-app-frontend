fdb3c56adbf1d6d95cbb44688f6759cd66987826
Audra Wathen <mrsbluerose@gmail.com>
2023-07-12 15:08:53 -0600

    TRIB-133: wip attempts to add data test selector to shared header button

--- src/app/pages/attributes-page/attributes.page.ts
+++ src/app/pages/attributes-page/attributes.page.ts
@@ -16,6 +16,7 @@ export class AttributesPage implements OnInit {
 
   headerPageTitle: string = 'Attributes';
   headerPagePrimaryActionButtonText: string = 'Create';
+  testDataText: string = 'launchCreateButton';
 
   constructor(private _userService: UserService,
     private _alertService: AlertService,
