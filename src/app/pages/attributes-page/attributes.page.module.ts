a9cf535c9f27f0e3e8cbdd5c43d32f2101710c47
Audra Wathen <mrsbluerose@gmail.com>
2023-07-18 18:40:08 -0600

    TRIB-133: adds shared header to attributes page - adds shared components to attributes page module - adds page title and action button properties to attributes .ts - adds get create button function to attributes .ts - adds shared header to attributes .html

--- src/app/pages/attributes-page/attributes.page.module.ts
+++ src/app/pages/attributes-page/attributes.page.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { AttributesPageRoutingModule } from './attributes.page-routing.module';
 
 import { AttributesPage } from './attributes.page';
+import { SharedComponentsModule } from '../../_shared-components/shared-components/shared-components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AttributesPageRoutingModule
+    AttributesPageRoutingModule, 
+    SharedComponentsModule
   ],
   declarations: [AttributesPage]
 })
