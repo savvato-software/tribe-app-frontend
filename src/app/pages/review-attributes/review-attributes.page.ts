8b88a6e7024dd95adb09587f42889a3ba5ae1573
Audra W <37217144+mrsbluerose@users.noreply.github.com>
2023-07-21 18:49:48 -0600

    TRIB-126: integrates input fields and btns to review phrase (#49)
    * TRIB-126: adds input and buttons to review attr.
    - adds read-only input to .html
    - adds button functions to .ts
    
    * TRIB-126: adds getNextPhrase method to rev. attr.
    - adds method with moc phrase object
    - adds method to convert to string
    - adds call to get method in .html input field value
    
    * TRIB-126: integrates getNextPhrase button function
    - updates Get Next Phrase to be enabled initially
    - updates Approve and Rejct buttons to be disabled initially
    - adds input field placeholder text with instructions to click Get Next Phrase
    - updates getNextPhraseToBeReviewed funciton to display phrase and manage
    button disable properties
    
    * TRIB-126: adds message to approve button
    - adds confirmation message to approve button click function
    - resets button disable properties to original values
    
    * TRIB-126: WIP adding alert for reject
    
    * TRIB-126: removes loading serv. from rev att .html
    
    * TRIB-126: WIP troubleshooting ion alert html tag
    
    * TRIB-126: replaces ionic reject alert with service
    - removes ionic alert .html
    - removes ionic alert values
    - adds alert service to reject button function
    
    * TRIB-126: refactors approve button function
    - removes msg variable
    - adds space to better read alert service code
    
    * TRIB-126: adds comment to specify mock data
    
    * TRIB-126: refactors approve reject buttons display
    - adds buttons to ion button group
    - creates single variable to determine if buttons are displayed
    
    * TRIB-126: integrated shared header
    - updated to shared header to match update to all other pages
    
    * TRIB-126: cypress test for page existence
    - create Cypress test file for review attributes page
    - adds data test label to home page button
    - adds test to see if page exists
    
    * TRIB-126 adds test for input field display holder
    
    * TRIB-126: tests that get phrase btn gets phrase
    - adds data test labels to get next phrase button and phrase to be
    reviewed input
    - verifies that the expected phrase appears
    
    * TRIB-126: tests that approve btn triggers alert
    - adds data test label to approve button
    - verifies that alert diplays
    - clicks okay and verifies it is dismissed
    
    * TRIB-126: tests that reject btn triggers alert
    - adds data test label to reject button
    - verifies that alert diplays
    - verifies and clicks a mock input
    - clicks okay and verifies it is dismissed
    
    * TRIB-126: logs user choice for rejecting phrase
    - adds handler to inputs to log user choice-

--- src/app/pages/review-attributes/review-attributes.page.ts
+++ src/app/pages/review-attributes/review-attributes.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AlertService } from '../../_services/alert/alert.service';
+
 @Component({
   selector: 'app-review-attributes',
   templateUrl: './review-attributes.page.html',
@@ -7,9 +9,100 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewAttributesPage implements OnInit {
 
-  constructor() { }
+  headerPageTitle: string = 'Review Attributes';
+
+  phraseToBeReviewed: String = "";
+  getNextPhraseButtonDisabled: boolean = false;
+  approveAndRejectButtonDisplayed: boolean = false;
+
+  constructor(private _alertService: AlertService) { }
 
   ngOnInit() {
+  }
+
+  onRejectPhraseBtnClick() {
+    //mock data
+    const reasons = [
+      {"id": 1, "reason": "approved"},
+      {"id": 2, "reason": "doesn't make sense"},
+      {"id": 3, "reason": "vulgar"}];
+
+    const self = this;
+
+    self._alertService.show({
+      header: 'Message rejected',
+      subheader: 'Choose a reason why:',
+      inputs: reasons.map((rsn) => {
+              return {
+                  type: 'radio',
+                  label: rsn.reason,
+                  value: rsn.reason,
+                  handler: (data) => {
+                    console.log('User choice: ', data.value);
+                  }
+              }
+          }, self),
+      buttons: [{
+        text: 'OK', role: 'cancel',
+        handler: () => {
+        }
+        }]
+    })
+
+    this.phraseToBeReviewed = "";
+    this.getNextPhraseButtonDisabled = false;
+    this.approveAndRejectButtonDisplayed = false;
+  }
+
+  onApprovePhraseBtnClick() {
+    const self = this;
+
+    self._alertService.show({
+    header: 'Message approved!',
+    buttons: [{
+      text: 'OK', role: 'cancel',
+      handler: () => {
+      }
+      }]
+    })
+
+    this.phraseToBeReviewed = "";
+    this.getNextPhraseButtonDisabled = false;
+    this.approveAndRejectButtonDisplayed = false;
+  }
+
+  onGetNextPhraseBtnClick() {
+    this.getNextPhraseToBeReviewed();
+  }
+
+  getNextPhraseToBeReviewed() {
+    //mock data
+    const phraseTBR = {"adverb": "competitively", "verb": "writes", "preposition": "nullvalue", "noun": "code" };
+
+    const phraseTBRAsString = this.getAttrString(phraseTBR);
+    this.phraseToBeReviewed = phraseTBRAsString;
+    this.getNextPhraseButtonDisabled = true;
+    this.approveAndRejectButtonDisplayed = true;
+  }
+
+  getAttrString(tbr) {
+    let rtn = "";
+
+    if (tbr.adverb)
+      rtn += tbr.adverb + " ";
+
+    rtn += tbr.verb + " ";
+
+    if (tbr.preposition)
+      rtn += tbr.preposition + " ";
+
+    rtn += tbr.noun;
+
+    return rtn;
+  }
+
+  getListOfReviewDecisionReasons() {
+    return
   }
 
 }
