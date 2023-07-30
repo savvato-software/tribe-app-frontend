2728a6b35220444e3d6b9584ac803934759e780b
Ashton Hanke <ashguy121@gmail.com>
2023-07-30 13:10:40 -0600

    updated save method to call backend and return response

--- src/app/pages/attributes-page/_services/attributes.api.service.ts
+++ src/app/pages/attributes-page/_services/attributes.api.service.ts
@@ -27,18 +27,29 @@ export class AttributesApiService {
 
     save(model) {
         const url = environment.apiUrl + '/api/attributes';
-        let data = { 'adverb': model['inputAdverbTxt'], 'verb': model['inputVerbTxt'], 'preposition': model['inputPrepositionTxt'], 'noun': model['inputNounTxt'] };
-
-        return new Promise(
-            (resolve, reject) => {
-                // this._apiService.post(url, data).subscribe(
-                //     (_data) => {
-                //         console.log('save attribute to server' + ' was successful --> ' + _data);
-                        resolve({ "successful": {status: true} });
-                    //     resolve({ "successful": _data });
-                    // }, (err) => {
-                    //     reject(err);
-                    // });
-            });
+        let data = {
+            'adverb': model['inputAdverbTxt'],
+            'verb': model['inputVerbTxt'],
+            'preposition': model['inputPrepositionTxt'],
+            'noun': model['inputNounTxt']
+        };
+    
+        return new Promise((resolve, reject) => {
+            this._apiService.post(url, data).subscribe(
+                (response: any) => {
+                    // Assume that the response contains a property named 'success' indicating whether the phrase is reviewed (true) or in review (false).
+                    // Adjust the property name as needed based on the actual response from the backend.
+                    const isPhraseReviewed = response.success;
+    
+                    // Resolve with the boolean value indicating whether the phrase is reviewed or in review
+                    resolve(isPhraseReviewed);
+                },
+                (err) => {
+                    // Handle any errors that occurred during the API call
+                    reject(err);
+                }
+            );
+        });
     }
+    
 }
