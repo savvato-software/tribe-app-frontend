e6e534734393ce531906b0d9e234a1f8d46f04ff
loretdemolas <sebast.loret@gmail.com>
2023-08-15 20:54:49 -0400

    notifications.api.service.ts -commented out deletenotification due to errors. It would not let the frontend load due to removal of mock api -Modified getMessages to use backend endpoint to get notifications notifications.page.html -modified conditional statement to use async pipe to wait for notification instead of measuring the mockapi array length notifications.page.ts -Silenced onDeleteNotification beause there is no notifcation due to removal of mockapi -added getUserID to find current users ID

--- src/app/pages/notifications-page/notifications.page.ts
+++ src/app/pages/notifications-page/notifications.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService } from './_service/notifications.api.service';
+import { Observable } from 'rxjs';
+import { AuthService } from '@savvato-software/savvato-javascript-services';
 
 @Component({
 selector: 'page-notification',
@@ -7,31 +9,36 @@ templateUrl: './notifications.page.html',
 styleUrls: ['./notifications.page.scss']
 })
 export class NotificationPage implements OnInit {
-notifications: any[];
+notifications$: Observable<any[]>;
 
-constructor(private notificationService: NotificationService) {}
+constructor(private notificationService: NotificationService,
+            private _authService:AuthService) {}
 
   ngOnInit() {
+    //Fake user ID where to get real id <<<<<<<<<<<
+    const userId = this.getUserId;
     // Call the getMessages() method of the notification service to load all notifications
-    this.notificationService.getMessages().subscribe(notifications => {
-      this.notifications = notifications;
-    });
-  }
-
-  getIcon(notification: any): string {
-    const icon = notification.icon;
+    this.notifications$ = this.notificationService.getMessages(userId);
+    }
+  
+  getIcon(notification$: any): string {
+    const icon = notification$.iconUrl;
     return icon ? icon : '';
   }
 
-  onDeleteNotification(id: number) {
-    // Call the deleteMessage() method of the notification service to delete a notification
-    this.notificationService.deleteMessage(id).subscribe(() => {
-      // Remove the deleted notification from the local array of notifications
-      this.notifications = this.notifications.filter(notification => notification.id !== id);
-    });
+  getUserId() {
+    return this._authService.getUser().id;
   }
 
-  onShowMoreInfo(notification: any) {
-    console.log('More information: ', notification);
+  //    onDeleteNotification(id: number) {                                                    <<<<<<<<<<<<<<< Need to send ID with notification?? <<<<<<<<<<<<<<<<
+    // Call the deleteMessage() method of the notification service to delete a notification
+  //  this.notificationService.deleteMessage(id).subscribe(() => {
+      // Remove the deleted notification from the local array of notifications
+  //    this.notifications = this.notifications.filter(notification => notification.id !== id);
+  //  });
+  //    }
+
+  onShowMoreInfo(notification$: any) {
+    console.log('More information: ', notification$);
   }
 }
