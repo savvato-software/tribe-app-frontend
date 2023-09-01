46956e7d166d70952ec4b0b4566d9504684d586f
loretdemolas <122843014+loretdemolas@users.noreply.github.com>
2023-08-31 21:51:22 -0400

    Trib 85 (#54)
    * TRIB-84: Implement notifications api:
    
    notifications.api.service.ts:
    
    Created a mock api that simulates data. It takes two arrays, one for item and the other for message. It builds the notifications with the correct icon:message pairing.
    
    notifications.page.html:
    
    Added an ion List that uses a for loop to create list items. Each item has an onlick event listern to remove the notification from the notification array and display. If no notifications are displayed, it will display "no new notificaion"
    
    notifications.page.ts:
    
    Added logic to handleDelete of the notificaion.
    
    Added a function to get notification icon and display it first in each list.
    
    Added oninit lifecycle to call the api service and get notitications.
    
    * TRIB-84: Implement notifications api pt2
    
    long-press.directive.ts:
    Created a directive that use mousedown, mouseleave, mouseup, and timout to implement a longpress click.
    
    notifications.page.module.ts:
    importated and declared directive
    
    notifications.page.html:
    Added longpress event listenter to call onDelete function.
    Added click event listerner to consol log more information on single click
    Added CSS class to give user feedback when longpressing.
    
    notifications.page.scss:
    Added CSS to change ionitem color. Currently doesn't work. For some reason, animations break the longpress directive.
    
    notifications.page.ts:
    Added showMore function to console.log more mock information on each notification.
    
    * notifications.api.service.ts
    -commented out deletenotification due to errors. It would not let the frontend load due to removal of mock api
    -Modified getMessages to use backend endpoint to get notifications
    notifications.page.html
    -modified conditional statement to use async pipe to wait for notification instead of measuring the mockapi array length
    notifications.page.ts
    -Silenced onDeleteNotification beause there is no notifcation due to removal of mockapi
    -added getUserID to find current users ID
    
    * _service/notifications.api.service.ts
    renamed class file to follow code style
    _service/notifications.model.service.ts
    created model service to carry data
    notifications.page.html
    modified html to use the new model
    notifications.page.ts
    Modified getAllNotificationForUser to use model
    
    * notifications.model.service.ts
    -changed the init to an async init to simplify code and await the response from api
    -changed model name to notifications to improve readability
    
    notifications.page.html
    -I was using the async pipe when i should of measured the array length. Aysnc pipe for some reason did not work anymore.
    notifications.page.ts
    -Discovered the getter to get the value from model service.
    
    made beautiful clean code.
    
    * notifications.page.html
    -Removed ion header and toolbar
    notifications.page.ts
    -Defined page header
    
    * _service/notifications.api.service.ts
    -changed promise to an async await
    /_service/notifications.model.service.ts
    -added space to the end of the code
    notifications.page.ts
    -added space to end of the code

--- src/app/pages/notifications-page/_service/notifications.api.service.ts
+++ src/app/pages/notifications-page/_service/notifications.api.service.ts
@@ -1,61 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { environment } from '../../../_environments/environment';
+import { HttpClient } from '@angular/common/http';
+import {AuthService, JWTApiService} from '@savvato-software/savvato-javascript-services';
 
 @Injectable({
     providedIn: 'root'
 })
-export class NotificationService {
-    //Data from MockAPI
-    private messagesWithIcons = [];
 
-    constructor() {
-        // Call buildMessage() to create an array of notifications with mockData
-        this.messagesWithIcons = this.buildMessage();
-    }
+export class NotificationApiService {
 
-    getMessages(): Observable<any[]> {
+    constructor(private _apiService: JWTApiService,
+                private _authService:AuthService) {}
 
-        return of(this.messagesWithIcons);
-    }
-
-    deleteMessage(id: number): Observable<any> {
-        // Find the index of the message to be deleted
-        const index = this.messagesWithIcons.findIndex(message => message.id === id);
-
-        // If the message exists, remove it from the array
-        if (index !== -1) {
-            this.messagesWithIcons.splice(index, 1);
+    async getAllNotificationsForUsers() {
+        const url = environment.apiUrl + '/api/notifications/user/' + this._authService.getUser().id;
+    
+        try {
+            const data = await this._apiService.get(url).toPromise();
+            return data;
+        } catch (error) {
+            throw error;
         }
-
-        // Return an observable with a response of 200 OK
-        return of({ status: 200 });
     }
+    
 
-    // Function to build an array of notifications with associated icons (MockAPI)
-    private buildMessage() {
-        //Messages for notification (MockAPI)
-        const notificationMessages = [
-            { id: 1, iconId: 1, text: 'Request Review from John' },
-            { id: 2, iconId: 2, text: 'Job Started by John' },
-            { id: 3, iconId: 4, text: 'Message from John: "What is a div?"' },
-            { id: 4, iconId: 3, text: 'Zoom call with John at 8:00pm est' },
-            { id: 5, iconId: 4, text: 'Message from John: "Hello, My name is John and I am an expert code writer!"' }
-        ];
-        //Icons for messages (MockAPI)
-        const notificationIcons = [
-            { iconId: 1, icon: 'notifications-outline' },
-            { iconId: 2, icon: 'mail-outline' },
-            { iconId: 3, icon: 'alarm-outline' },
-            { iconId: 4, icon: 'chatbubble-outline' },
-        ];
-
-        // Loop to combine messages and icons
-        const notifications = notificationMessages.map(message => {
-            const icon = notificationIcons.find(icon => icon.iconId === message.iconId)?.icon;
-            return { ...message, icon };
-        });
-
-        return notifications;
-        console.log(notifications)
-    }
+    // deleteMessage(id: number): Observable<any> {}
 }
