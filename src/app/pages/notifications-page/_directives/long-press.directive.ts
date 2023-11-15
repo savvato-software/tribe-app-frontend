1216e4192314a3131f6d61301a225014fc2028d3
loretdemolas <sebast.loret@gmail.com>
2023-11-14 21:43:51 -0500

    long-press.directive.ts -Added logic to set loading bar state on long press objects notifications.page.scss -added conditional css to animate a loading bar when loading bar state is set to true.

--- src/app/pages/notifications-page/_directives/long-press.directive.ts
+++ src/app/pages/notifications-page/_directives/long-press.directive.ts
@@ -1,33 +1,67 @@
 import { Directive, EventEmitter, HostBinding, HostListener, Output } from '@angular/core';
 
+
+const longPressDuration = 1200; // Set the long press duration in milliseconds
+const loadingBarDelay = 200; // Set the delay for loading bar appearance in milliseconds
+
 @Directive({
   selector: '[appLongPress]'
-})
-export class LongPressDirective {
-  @Output()
-  longPress = new EventEmitter();
+})  
 
-  private timeout: any;
+export class LongPressDirective {
+  @Output() longPress = new EventEmitter();
+
+  private startTime: number;
+  private loadingBarTimeout: any;
+  private longPressTimeout: any;
+
 
   @HostBinding('class.long-press') isPressed = false;
+  @HostBinding('class.loading-bar') loadingBar = false;
 
   @HostListener('mousedown') onMouseDown() {
-    console.log('mousedown');
     this.isPressed = true;
-    this.timeout = setTimeout(() => {
-      this.longPress.emit();
-    }, 1000); // set the long press time to 1 second
+    this.startTime = Date.now();
+
+
+    this.loadingBarTimeout = setTimeout(() => {
+      if (this.isPressed) {
+        this.loadingBar = true; // Show loading bar after the delay
+      }
+    }, loadingBarDelay);
+
+    this.longPressTimeout = setTimeout(() => {
+      if (this.isPressed) {
+        this.longPress.emit();
+        this.resetState();
+      }
+    }, longPressDuration);
   }
 
-    @HostListener('mouseup') onMouseUp() {
-      console.log('mouseup');
-      this.isPressed = false;
-      clearTimeout(this.timeout);
+  @HostListener('mouseup') onMouseUp() {
+    this.emitOrReset();
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.emitOrReset();
+  }
+
+  private emitOrReset() {
+    clearTimeout(this.loadingBarTimeout);
+    clearTimeout(this.longPressTimeout);
+
+    if (this.isPressed && this.loadingBar) {
+      const elapsed = Date.now() - this.startTime;
+      if (elapsed >= longPressDuration) {
+        this.longPress.emit();
+      }
     }
 
-    @HostListener('mouseleave') onMouseLeave() {
-      console.log('mouseleave');
-      this.isPressed = false;
-      clearTimeout(this.timeout);
-    }
+    this.resetState();
+  }
+
+  private resetState() {
+    this.isPressed = false;
+    this.loadingBar = false;
+  }
 }
