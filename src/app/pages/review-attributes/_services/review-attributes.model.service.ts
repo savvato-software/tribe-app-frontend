7c61640caae295f0c5282125b42679e76895dcc9
Albey Kappil <50112186+albkappil@users.noreply.github.com>
2023-11-28 15:08:36 -0600

    Trib -128: Review approve attributes (#62)
    * approve code for reviewing attributes

--- src/app/pages/review-attributes/_services/review-attributes.model.service.ts
+++ src/app/pages/review-attributes/_services/review-attributes.model.service.ts
@@ -1,33 +1,52 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import {AuthService, JWTApiService} from '@savvato-software/savvato-javascript-services';
 import { ReviewAttributesApiService } from "./review-attributes.api.service";
-import { ModelTransformingService } from "@savvato-software/savvato-javascript-services";
+import { Constants } from "../../../_constants/constants";
 
 @Injectable({
-  providedIn: "root",
+    providedIn: 'root'
 })
 export class ReviewAttributesModelService {
-  model: any = {};
 
-  constructor(
-    private _modelTransformingService: ModelTransformingService,
-    private _reviewAttributesApiService: ReviewAttributesApiService
-  ) {}
+    model: any = {};
+    reasonList: any = [];
+    saveReview: any = {};
 
-  init() {
-    this._modelTransformingService.clearAllTransformers();
+    constructor(private _apiService: JWTApiService,
+        private _authService: AuthService,
+        private ReviewAttributesApiService: ReviewAttributesApiService,
+        private _constants: Constants) {
 
-    this._modelTransformingService.addTransformer((model, done) => {
-      this._reviewAttributesApiService
-        .getNextReviewEligiblePhrase()
-        .then((response) => {
-          model["nextReviewPhrase"] = response;
-          done();
+    }
+
+
+    getNewPhrase() {
+        return this.ReviewAttributesApiService.getPhrase().then((response) => {
+            this.model = response;
+        })
+        
+        
+    }
+
+    getReasonList() {
+        return this.ReviewAttributesApiService.getReasonListApi().then((response) => {
+            this.reasonList = response;
+        })
+         
+    }
+
+    saveReviewAttributes(reasonId) {
+        console.log(this.model.id);
+        this.saveReview['reviewId'] = this.model.toBeReviewedId;
+        this.saveReview['userId'] = this._authService.getUser().id;
+        this.saveReview['reasonId'] = reasonId; 
+        return this.ReviewAttributesApiService.saveRA(this.saveReview).then(() =>{
+            console.log("Succesfully Saved Review Attributes");
         });
-    });
-    this._modelTransformingService.transform(this.model);
-  }
+            
+    }
 
-  getNextReviewEligiblePhrase() {
-    return this.model["nextReviewPhrase"];
-  }
+    get() {
+        return this.model;
+    }
 }
