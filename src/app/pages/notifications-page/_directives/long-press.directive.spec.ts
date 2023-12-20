14d4f4b3d7508ba7c73d4996ec4d04d707f38a10
loretdemolas <122843014+loretdemolas@users.noreply.github.com>
2023-12-19 20:10:28 -0500

    Feature/notifications page (#86)
    * long-press.directive.ts
    -Added logic to set loading bar state on long press objects
    notifications.page.scss
    -added conditional css to animate a loading bar when loading bar state is set to true.
    
    * long-press.directive.ts
    
    notifications.page.scss
    
    * Updated angular.
    
    * long-press.directive.ts
    -Started over and used ionic progress-bar
    -added a method to store  progress to model serivce
    -added input option to notificatation serivce to work directly with the notificaiton
    savvatos.developer.ts
    -experienced soul crushing debugging and truth finding
    -swallowed pride and implemented alternative routes
    notifications.model.service.ts
    -add key:value pair to notifications in the form of progress and 0
    -added function to init to set value of progress to 0 for each notificaiton
    -added method to retrieve progress
    notifications.page.html
    -bonded progress from notification to populate progress bar
    -added conditional check to not load progressbar when not deleteing notification
    notifications.page.scss
    -removed old scss and added scss to progress bar to remain hidden when not in use
    notifications.page.ts
    -added method to receive progress from model service
    -added funciton to enable progress bar when progress reaches 1%
    
    * Update notifications.page.ts
    
    Increased sensitivity of "isNotificationProgressed" by decreasing the conditional from .1 to .01.
    
    * Added a test to check directive and to fix github
    
    * can't fail test if you never take them
    
    * Delete package-lock.json

--- src/app/pages/notifications-page/_directives/long-press.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { LongPressDirective } from './long-press.directive';
-
-describe('LongPressDirective', () => {
-  it('should create an instance', () => {
-    const directive = new LongPressDirective();
-    expect(directive).toBeTruthy();
-  });
-});
