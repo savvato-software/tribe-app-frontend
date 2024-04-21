5e91edcedf739c8a801d319f2f892aa0d6c3445e
Ashton Hanke <82555871+downbythesieh@users.noreply.github.com>
2024-04-21 13:15:38 -0600

    Trib 103 clicking on a connection in list-connections brings you to connection-details page (#97)
    * created connection-details pages
    
    * created html with cancel button
    
    * created details.ts page with button navigation
    
    * added link to details page using list connection elements
    
    * updated routing module to reach connection details page
    
    * updated list connections module to include connection details
    
    * created scss page
    
    * changed name of scss page

---
+++ src/app/pages/connect-page/connection-details/connection-details.page.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute} from "@angular/router";
+import { Router} from "@angular/router";
+import { ConnectModelService } from '../_services/connect.model.service';
+
+@Component({
+    selector: 'app-connection-details',
+    templateUrl: './connection-details.page.html',
+    styleUrls: ['./connection-details.scss']
+})
+export class ConnectionDetailsPage implements OnInit{
+    userId: string;
+    connectionDetails: any;
+
+    constructor( 
+                private route: ActivatedRoute,
+                private connectModelService: ConnectModelService,
+                private _router: Router) {}
+
+    ngOnInit() {}
+
+    onCancelBtnClick() {
+        this._router.navigate(['/connect/list-connections']);
+    }
+
+    // getCancelBtnClickFunc() {
+    //     const self = this;
+    //     return () => {
+    //       self.navigateTo('/list-connections');
+    //     }
+    // }
+    
+
+    navigateTo(url?: string) {
+        this._router.navigate([url], { replaceUrl: true });
+    }
+}
