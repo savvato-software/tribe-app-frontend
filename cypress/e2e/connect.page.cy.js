40f9f9b1b25bde89cb8fb0cc97ef8ae249a64524
Johnathan James <haxwell@gmail.com>
2024-09-06 12:13:03 -0600

    Getting cypress tests for the connection page to be mocked.

--- cypress/e2e/connect.page.cy.js
+++ cypress/e2e/connect.page.cy.js
@@ -1,8 +1,64 @@
-it('Connect page should have accept new connection button', () => {
+describe('Connections Page', () => {
+    Cypress.Commands.add('goToConnectionsPage', () => {
+        cy.appLogin();
+        cy.get('[data-test="connect-menu-item"]').click();
+    });
 
-    cy.visit('http://localhost:8100/login')
-    cy.get('[data-test="sign-in-btn"]').click()
-    cy.get('[data-test="launchConnectPageButton"]').click()
-    cy.get('[data-test="acceptNewConnectionBtn"]').click()
-    cy.visit('http://localhost:8100/connect/accept-new-connection')
+    it('navigate to connections page', () => {
+        cy.goToConnectionsPage();
+    });
+
+    it('should display connections page', () => {
+        cy.goToConnectionsPage();
+        cy.get('.listbtn').should('have.length', 1);
+        cy.get('.openbtn').should('have.length', 1);
+        cy.get('.acceptbtn').should('have.length', 1);
+    });
+
+    describe('List Connections Page', () => {
+        Cypress.Commands.add('goTolistConnectionPage', () => {
+            cy.goToConnectionsPage();
+
+            // get a component with the class "listbtn" and click it
+            cy.get('.listbtn').click();
+        });
+
+        const userId = '12345';
+
+        it('should display list connections page', () => {
+            cy.intercept('GET', 'http://localhost:8080/api/connect/' + userId + '/all', (req) => {
+                req.reply({
+                    statusCode: 200,
+                    body: [{
+                            to: {
+                                userId: '344',
+                                username: 'testuser1'
+                            }
+                        },
+                        {
+                            to: {
+                                userId: '345',
+                                username: 'testuser2',
+                            }
+                        }, {
+                            to: {
+                                userId: '346',
+                                username: 'testuser3',
+                            }
+                        }]
+                });
+            }).as('getAllConnections');
+
+            cy.goTolistConnectionPage();
+            cy.get('[data-test="connectionListItem"]').should('have.length', 3);
+        });
+    });
+
+    describe('Accept New Connection Page', () => {
+
+    });
+
+    describe('Open Connection Page', () => {
+
+    });
   })
