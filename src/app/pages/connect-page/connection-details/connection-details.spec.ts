87a500f9a38813a32a35819c6d6c4c04df027d9a
Jacob Snell <jake.r.snell@gmail.com>
2024-09-09 17:14:17 -0600

    update tests for passing 'shouldCreate'

--- src/app/pages/connect-page/connection-details/connection-details.spec.ts
+++ src/app/pages/connect-page/connection-details/connection-details.spec.ts
@@ -4,6 +4,31 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import {ConnectionDetailsPage} from "./connection-details.page";
 import {Constants} from "../../../_constants/constants"; // Import HttpClientTestingModule
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
+import { AuthService, JWTApiService } from '@savvato-software/savvato-javascript-services';
+import { ConnectApiService } from '../_services/connect.api.service';
+import { ConnectModelService } from '../_services/connect.model.service';
+
+class MockConnectModelService {
+  model :any[] = [
+    {
+      "connectionError": null,
+      "connectionSuccess": true,
+      "message": "",
+      "to": {
+        "userId": 11,
+        "username": "testuser10",
+        "userConnectionStatus": "requesting"
+      }
+    },
+  ];
+
+  getAllConnections() :any[] {
+    return this.model;
+  }
+
+}
 
 describe('ConnectionDetailsPage', () => {
   let component: ConnectionDetailsPage;
@@ -13,7 +38,16 @@ describe('ConnectionDetailsPage', () => {
     TestBed.configureTestingModule({
       declarations: [ ConnectionDetailsPage ],
       providers: [
-        { provide: Constants, useClass: Constants }
+        { provide: Constants, useClass: Constants },
+        { provide: ConnectModelService, useClass: MockConnectModelService },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({
+              userId: 11
+            })
+          }
+        }
       ],
       imports: [
         HttpClientTestingModule, // Use HttpClientTestingModule instead of HttpClientModule
@@ -24,7 +58,7 @@ describe('ConnectionDetailsPage', () => {
 
     fixture = TestBed.createComponent(ConnectionDetailsPage);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.connectedWithUserId = 11;
   }));
 
   it('should create', () => {
