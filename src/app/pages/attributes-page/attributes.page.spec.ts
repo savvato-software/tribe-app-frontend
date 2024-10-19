134281aee92bd5d187d497676b64c8287e206611
Jason Jacobs <56858451+Jayj30pro@users.noreply.github.com>
2024-10-19 12:19:30 -0600

    TRIB-140: Write Happy Path Unit Tests for permissions.api.service.ts  (#155)
    * Loading Fixed
    
    * all test pased

--- src/app/pages/attributes-page/attributes.page.spec.ts
+++ src/app/pages/attributes-page/attributes.page.spec.ts
@@ -7,17 +7,35 @@ import {HttpClientModule} from "@angular/common/http";
 
 import { Constants } from '../../_constants/constants';
 import {SharedComponentsModule} from "../../_shared-components/shared-components/shared-components.module";
+import { LoadingService } from '../../_services/loading-spinner/loading.service';
+import { AttributesModelService } from "./_services/attributes.model.service";
+import { of } from 'rxjs';
 
 describe('AttributesPage', () => {
   let component: AttributesPage;
   let fixture: ComponentFixture<AttributesPage>;
+  let loadingServiceSpy: jasmine.SpyObj<LoadingService>;
+  let attributesModelServiceSpy: jasmine.SpyObj<AttributesModelService>;
 
   beforeEach(waitForAsync(() => {
+    loadingServiceSpy = jasmine.createSpyObj('LoadingService', ['show', 'dismiss']);
+    attributesModelServiceSpy = jasmine.createSpyObj('AttributesModelService', ['init', 'get', 'isDirty']);
+    
+    loadingServiceSpy.show.and.returnValue(Promise.resolve());
+    loadingServiceSpy.dismiss.and.returnValue(Promise.resolve());
+    attributesModelServiceSpy.init.and.returnValue(Promise.resolve());
+    attributesModelServiceSpy.get.and.returnValue([]);
+    attributesModelServiceSpy.isDirty.and.returnValue(false);
+
     TestBed.configureTestingModule({
       declarations: [ AttributesPage ],
       providers: [
-        { provide: Constants, useClass: Constants }
+        { provide: Constants, useClass: Constants },
+        { provide: LoadingService, useValue: loadingServiceSpy },
+        { provide: AttributesModelService, useValue: attributesModelServiceSpy }
       ],
+        
+      
       imports: [HttpClientModule, SharedComponentsModule, RouterTestingModule.withRoutes([]), IonicModule.forRoot()]
     }).compileComponents();
 
